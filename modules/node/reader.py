aa7e745a18f61dfc91a4fe8bbc1193f13a7e0095
Gwyndolin <danil.zhuravlev.2015@gmail.com>
2026-01-08 14:11:06 +0400

    feat: Добавлена поддержка конфигурации формата и размера кадров в VideoReader
    Добавлены параметры конфигурации target_format и target_size для управления обработкой кадров. Заменен multiprocessing.Queue на queue.Queue

--- modules/node/reader.py
+++ modules/node/reader.py
@@ -4,7 +4,7 @@ from dataclasses import dataclass
 import numpy as np
 import cv2
 import threading
-from multiprocessing import Queue
+import queue
 
 @dataclass
 class FramePayload:
@@ -14,9 +14,11 @@ class FramePayload:
 class VideoReader:
     default_conf = OmegaConf.create({
         "video_src":"cv",
+        "target_format":"rgb",
+        "target_size":"orig"
     })
 
-    def __init__(self, conf: DictConfig, frame_queue: Queue[FramePayload]):
+    def __init__(self, conf: DictConfig, frame_queue: queue.Queue):
         self.conf = OmegaConf.merge(self.default_conf, conf)
 
         self.stop_event = threading.Event()
@@ -38,8 +40,23 @@ class VideoReader:
             ret, frame = self.cap.read()
             if not ret:
                 continue
-            frame_payload = FramePayload(frame=frame, timestamp=time.monotonic())
-            self.frame_queue.put(frame_payload)
+
+            if self.conf.target_format == "rgb":
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            if self.conf.target_size == "orig":
+                frame_payload = FramePayload(frame=frame, timestamp=time.monotonic())
+            else:
+                frame_payload = FramePayload(frame=cv2.resize(frame, self.conf.target_size), timestamp=time.monotonic())
+            
+            try:
+                self.frame_queue.put(frame_payload, block=False)
+            except queue.Full:
+                try:
+                    self.frame_queue.get_nowait()
+                except queue.Empty:
+                    pass
+                self.frame_queue.put(frame_payload, block=False)
             
     def stop(self):
         self.stop_event.set()
