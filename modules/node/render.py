7140f5863f6fbe3dec0f8f697f07c741e4a0bcb7
Gwyndolin <danil.zhuravlev.2015@gmail.com>
2026-01-08 12:42:13 +0400

    feat: Add Render node for processing tracker queue data

---
+++ modules/node/render.py
@@ -0,0 +1,23 @@
+from omegaconf import DictConfig, OmegaConf
+from dataclasses import dataclass
+from multiprocessing import Queue
+from modules.node.tracker import TrackerPayload
+import threading
+
+class Render:
+    default_conf = OmegaConf.create({
+        "sus":True,
+    })
+    def __init__(self, conf: DictConfig, tracker_queue: Queue[TrackerPayload]):
+        self.conf = OmegaConf.merge(self.default_conf, conf)
+        self.tracker_queue = tracker_queue
+        self.stop_event = threading.Event()
+    
+    def start(self):
+        self.stop_event.clear()
+        while not self.stop_event.is_set():
+            tracker_payload = self.tracker_queue.get()
+            pass
+
+    def stop(self):
+        self.stop_event.set()
