fbf4f51abebce996796edf1a0c4d8584794f3a1b
Gwyndolin <danil.zhuravlev.2015@gmail.com>
2026-01-08 12:41:56 +0400

    feat: Add VideoReader node for frame capture and queue management

---
+++ modules/node/reader.py
@@ -0,0 +1,47 @@
+import time
+from omegaconf import DictConfig, OmegaConf
+from dataclasses import dataclass
+import numpy as np
+import cv2
+import threading
+from multiprocessing import Queue
+
+@dataclass
+class FramePayload:
+    frame: np.ndarray
+    timestamp: float
+
+class VideoReader:
+    default_conf = OmegaConf.create({
+        "video_src":"cv",
+    })
+
+    def __init__(self, conf: DictConfig, frame_queue: Queue[FramePayload]):
+        self.conf = OmegaConf.merge(self.default_conf, conf)
+
+        self.stop_event = threading.Event()
+        self.frame_queue = frame_queue
+        
+        if self.conf.video_src == "cv":
+            self.cap = cv2.VideoCapture(0)
+        else:
+            self.cap = cv2.VideoCapture(0) # TODO: add video src support
+
+    def start(self):
+        self.stop_event.clear()
+
+        if self.conf.video_src == "cv":
+            self.__start_cv_frame_src()
+    
+    def __start_cv_frame_src(self):
+        while not self.stop_event.is_set():
+            ret, frame = self.cap.read()
+            if not ret:
+                continue
+            frame_payload = FramePayload(frame=frame, timestamp=time.monotonic())
+            self.frame_queue.put(frame_payload)
+            
+    def stop(self):
+        self.stop_event.set()
+
+        
