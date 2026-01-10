cebe0d9c462c928e524294e9c08caf5da520685f
danil <danil.zhuravlev.2015@gmail.com>
2026-01-10 23:29:26 +0300

    feat: Добавлен модуль node с экспортом основных компонентов обработки

---
+++ modules/node/__init__.py
@@ -0,0 +1,11 @@
+from .reader import VideoReader, FramePayload
+from .tracker import Tracker, TrackerPayload
+from .render import Render
+
+__all__ = [
+    "VideoReader",
+    "FramePayload",
+    "Tracker",
+    "TrackerPayload",
+    "Render",
+]
